be87af9430dbb644e3da0fe80b2f8bdcbf5bc59a
Marcos Henrique Reichert <mark21.reichert@gmail.com>
2022-01-31 17:29:45 -0300

    First Shiny Dashboard Code

--- DCdashboard.R
+++ DCdashboard.R
@@ -1,5 +1,22 @@
-##Limpeza dos dados
+##Cleaning Leftover Data
 rm(list=ls())
 
 my.dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(my.dir)
+
+##Loading packages
+library(shiny)
+library(shinydashboard)
+
+#Creating the header, sidebar and body
+header <- dashboardHeader()
+sidebar <- dashboardSidebar() 
+body <- dashboardBody()
+
+#Creating the UI
+
+ui <- dashboardPage(header, sidebar, body, title = 'TESTE', skin = 'blue')
+
+server <- function(input, output) {}
+
+shinyApp(ui, server)
