2f4b94e5f5d33af5edd4f582dd4e47ad033a1143
Marcos Henrique Reichert <mark21.reichert@gmail.com>
2022-01-31 18:35:38 -0300

    First part of the course

--- DCdashboard.R
+++ DCdashboard.R
@@ -9,9 +9,37 @@ library(shiny)
 library(shinydashboard)
 
 #Creating the header, sidebar and body
-header <- dashboardHeader()
-sidebar <- dashboardSidebar() 
-body <- dashboardBody()
+header <- dashboardHeader(
+  dropdownMenu(type = 'messages',
+               messageItem(
+                 from = 'Marcos',
+                 message = 'Gremio maior do mundo',
+                 href = 'https://socio.gremio.net/'
+               )
+               )
+)
+
+sidebar <- dashboardSidebar(
+                    sidebarMenu(
+                          menuItem(text = 'Data', tabName = 'data'),
+                          menuItem(text = 'Dashboard', tabName = 'dashboard')
+                              )
+                            ) 
+
+body <- dashboardBody(
+                      tabItems(
+                      tabItem(tabName = 'data',
+                              tabBox(
+                                title = "O Gremio é:",
+                                tabPanel("Gigante"),
+                                tabPanel("Imortal")
+                              )), 
+                      tabItem(tabName = 'dashboard',
+                              'Gremio maior do mundo')
+                    )
+                    )
+
+dropdownMenu()
 
 #Creating the UI
 
